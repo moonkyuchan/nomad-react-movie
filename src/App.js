8fb79fe6d8fa411a9c00380eca6bbddb7aaa466e
문규찬 <lazychan90@gmail.com>
2021-07-27 14:56:14 +0900

    라이프 사이클 간단 정리

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodArr = [
-  { name: "김치", price: 1000, rating: 3.4 },
-  { name: "비빔밥", price: 2000, rating: 3.8 },
-  {
-    name: " 부대찌개",
-    price: 12000,
-    rating: 4.2,
-  },
-];
-
-function Food({ name, price, rating }) {
-  // 인자로 porps 를 받아 아래에 {props.food}로도 사용가능
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h2 style={{ color: "red" }}>pirce is {price}</h2>
-      <h2 style={{ color: "blue" }}>rating is {rating} / 5 </h2>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div>
-      {foodArr.map((item, idx) => {
-        return (
-          <Food
-            key={idx}
-            name={item.name}
-            price={item.price}
-            rating={item.rating}
-          />
-        );
-      })}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    console.log("constructor");
+  }
+  //React는 자동적으로 Class component의 render method를 실행한다.
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState({ count: this.state.count + 1 }); //setState를 불러와 안에 state를 건드린다.
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  // componentDidMount() {
+  //   console.log("componentDidMount");
+  // }
+  // componentDidUpdate() {
+  //   console.log("componentDidUpdate");
+  // }
+  // componentWillUnmount() {
+  //   console.log("componentWillUnmount");
+  // }
+  render() {
+    // console.log("render function");
+    //setState를 호출하면 react는 state를 refresh하고 또한 render()를 다시 호출한다.
+    return (
+      <div>
+        <h1>the Number is {this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// react 라이프사이클
+// 1. constructor
+// 2. render
+// 3. componentDidMount - 렌더로 한번 그려지고 컴디마는 어디에 사용하는걸까?
+// 4. state가 변경되면 -> render 한번 돌고 -> componentDidUpdate
+// 5. componentWillUnmount 는 컴포넌트 kill!!!!할때
